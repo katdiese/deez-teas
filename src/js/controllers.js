203c530ec6eb00ec672e9d365bef9ae7f0777ac2
Kathy Dieser <katdiese@gmail.com>
2016-04-08 16:10:17 -0600

    connected services, put in tea data, created return function

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,3 +1,9 @@
-app.controller('myController', ['$scope', function($scope) {
-  $scope.greeting = "Hello World!";
+app.controller('teasController', ['$scope', 'teaService', function($scope, teaService) {
+  $scope.message = "I am the teas controller!"
+  $scope.firstTea = teaService.getTeas();
 }]);
+
+app.controller('cartController', ['$scope', 'teaService', function($scope, teaService) {
+  $scope.message = "I am the cart controller!"
+}])
+
