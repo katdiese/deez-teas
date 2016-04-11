60c8c7400f614ebf9802f6c7de8b0cf7de95d806
Kathy Dieser <katdiese@gmail.com>
2016-04-10 21:58:01 -0600

    created calc function for total, created custom filter for prices

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,20 @@ app.controller('teasController', ['$scope', 'teaService', function($scope, teaSe
   $scope.addToCart = function(item, quantity) {
     teaService.addToCart(item, quantity);
   }
+  $scope.itemsInCart = function() {
+    return teaService.getCart().length;
+  }
 }]);
 
 app.controller('cartController', ['$scope', 'teaService', function($scope, teaService) {
-  $scope.message = "I am the cart controller!"
+  $scope.message = "I am the cart controller!";
+  $scope.getCart = teaService.getCart();
+  $scope.totalCost = 0;
+  $scope.calcTotal = function(cart) {
+    cart.map(function(el) {
+      $scope.totalCost += el.price;
+    })
+  }
+  $scope.calcTotal($scope.getCart);
 }])
 
